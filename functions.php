d25367b9a0d910b4d4fc4ec4f145b1a3df96a83f
eiabadmin <webteam@eiab.eu>
2013-03-17 19:12:50 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+/* Leaf child theme starter
+*
+* When using a child theme (see http://codex.wordpress.org/Theme_Development and
+* http://codex.wordpress.org/Child_Themes), you can override certain functions
+* (those wrapped in a function_exists() call) by defining them first in your child theme's
+* functions.php file. The child theme's functions.php file is included before the parent
+* theme's file, so the child theme functions would be used.
+*
+* Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+* to a filter or action hook. The hook can be removed by using remove_action() or
+* remove_filter() and you can attach your own function to the hook.
+*
+* We can remove the parent theme's hook only after it is attached, which means we need to
+* wait until setting up the child theme:
+*
+*<code>
+*add_action( 'after_setup_theme', 'my_child_theme_setup' );
+*function my_child_theme_setup() {
+* // We are providing our own filter for excerpt_length (or using the unfiltered value)
+* remove_filter( 'excerpt_length', 'sonar_excerpt_length' );
+* ...
+* }
+* </code>
+*
+* For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+*
+* ###########################-- Start editing Below --###############################
+*/
+
+
+function my_scripts_method() {
+       wp_register_script( 'my-js-file',
+           get_template_directory_uri() . '/js/my-js.js',
+           array( 'jquery' ),
+           '1.0',
+           false );
+
+       wp_enqueue_script( 'my-js-file' );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+
+
+
+
+?>
